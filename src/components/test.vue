96912bf3d206c299d69ba69838e95917e16aa757
qw19696 <429558984@qq.com>
2022-02-10 13:47:47 +0800

    Changes

---
+++ src/components/test.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(x,index) in tdata" :key='index'>{{x}}</li>
+        </ul>
+        <h1>{{count}}</h1>
+        <button @click="up">+</button><hr>
+        <button @click="reduce">-</button>
+    </div>
+</template>
+
+<script>
+    export default{
+        data(){
+            return{
+            tdata:['1','2','3','4'],
+            count:0,
+            }
+        },
+        methods:{
+          up(){
+            this.count++
+          },
+          reduce(){
+            this.count--
+          }
+        }
+    }
+</script>
