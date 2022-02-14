64963d16f05cbf04de2f876cecf9eb4ab0d3ed6f
qw19696 <429558984@qq.com>
2022-02-14 16:33:46 +0800

    移除无用文件

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
 <template>
   <div id="app">
-    <test :name='msg'></test>
-    <input type="text" v-model="msg">
-    <button @click="test">测试按钮</button>
 
   </div>
 </template>
 
 <script>
 
-import test from "@/components/test";
+
 
 export default {
   name: 'App',
   components: {
-    test
+
   },
   data(){
     return{
-      msg:'李四'
+
     }
   },
   methods:{
-    test(){
-      this.$children[0].ceshi()
-    }
+
+
   }
 }
 </script>
